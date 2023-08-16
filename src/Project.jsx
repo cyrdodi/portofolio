528122c07da1614aa5caeb768591f8f51b63737d
Dodi Yulian <cdoditri@gmail.com>
2023-08-16 10:21:18 +0700

    project done

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,13 +1,13 @@
-const Project = () => {
+const Project = (props) => {
   return (
     <div className='px-4 py-2 my-4 transition ease-in-out delay-75 border rounded-lg text-green-50 border-green-50 hover:text-green-100 hover:bg-green-800'>
-      <div className="mb-4 text-2xl font-semibold">Project Name</div>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam beatae, incidunt magnam rerum ratione omnis temporibus autem, explicabo reprehenderit nam, nulla doloremque sint illo. Beatae nostrum doloribus neque hic sequi, ex illo.</p>
-      <div className="mt-2">Link: <a href="" className="underline">project-url.com</a></div>
-      <p className="mt-2 font-semibold">Stacks:</p>
+      <div className="mb-4 text-2xl font-semibold">{props.name}</div>
+      <p>{props.description}</p>
+      {props.link && <div className="mt-2">Link: <a href={props.link} target="_blank" className="underline">{props.link}</a></div>}
+      <p className="mt-2 mb-3 font-semibold">Stack:</p>
       <ul className="flex gap-2">
-        <li className="px-2 py-0.5 text-sm bg-green-300 text-green-950 rounded-2xl">Laravel</li>
-        <li className="px-2 py-0.5 text-sm bg-green-300 rounded-2xl text-green-950">Laravel</li>
+        {props.stack.map(item => <li className="px-2 py-0.5 text-sm bg-green-300 text-green-950 rounded-2xl">{item}</li>)}
+
       </ul>
     </div>
   )
