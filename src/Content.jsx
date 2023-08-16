940cb04fcd0000a2ed4a4960b254d8dbf1992287
Dodi Yulian <cdoditri@gmail.com>
2023-08-16 11:27:24 +0700

    fix: mobile viewport

--- src/Content.jsx
+++ src/Content.jsx
@@ -41,24 +41,27 @@ const Content = () => {
 
   const projects = [
     {
-      name: 'E-Kaku v2',
-      link: 'https://e-kaku.pandeglangkab.go.id',
-      description: 'Aplikasi pembuatan kartu kuning di Dinas Transmigrasi dan Ketenagakerjaan Kab. Pandeglang',
-      stack: [
-        'Laravel', 'Tailwind CSS'
-      ]
-    },
-    {
       name: 'SIMINAW',
       link: 'https://siminaw.pandeglangkab.go.id',
       description: 'Aplikasi manajemen inventaris Aplikasi dan Website di Diskomsantik Kab. Pandeglang',
+      year: '2023',
       stack: [
         'Laravel', 'Filament PHP', 'Tailwind CSS'
       ]
     },
     {
+      name: 'E-Kaku v2',
+      link: 'https://e-kaku.pandeglangkab.go.id',
+      description: 'Aplikasi pembuatan kartu kuning di Dinas Transmigrasi dan Ketenagakerjaan Kab. Pandeglang',
+      year: '2022',
+      stack: [
+        'Laravel', 'Tailwind CSS'
+      ]
+    },
+    {
       name: 'e-Hom',
       description: 'Elektronik Handover',
+      year: '2022',
       stack: [
         'Codeignigter 3', , 'Tailwind CSS'
       ]
@@ -66,13 +69,15 @@ const Content = () => {
     {
       name: 'REMI',
       description: 'Penilaian Remunerasi Elektronik RS Misi Lebak',
+      year: '2022',
       stack: [
         'Codeignigter 3', , 'Tailwind CSS'
       ]
     },
     {
       name: 'DOMI',
-      description: 'Pendaftaran Online DOMI',
+      description: 'Pendaftaran Online Rawat Jalan dan Rawat Inap RS Misi Lebak',
+      year: '2021',
       link: 'https://domi.misilebak.com',
       stack: [
         'Codeignigter 3', 'Bootstrap CSS'
@@ -83,7 +88,8 @@ const Content = () => {
 
   return (
     <div className='text-green-50'>
-      <section className="flex justify-between max-w-6xl gap-8 mx-auto mt-32">
+      <section className="justify-between max-w-6xl gap-8 mx-auto mt-14 md:mt-32 md:flex">
+        <img className="mb-12 rounded-full w-72 h-72 md:mb-0" src={logo} alt="Photo of Dodi Yulian" />
         <div>
           <div className='text-6xl font-bold '>Halo 👋, Saya <span className='text-blue-50'>Dodi Yulian</span></div>
           <p className='mt-4 mb-2 text-3xl font-semibold text-green-200'>Web Developer</p>
@@ -95,15 +101,13 @@ const Content = () => {
             {socialBadge.map((item) => <SocialBadge href={item.href} logo={item.logo} alt={item.alt} />)}
           </div>
         </div>
-        <div>
-          <img className="rounded-full w-72" src={logo} alt="Photo of Dodi Yulian" />
-        </div>
+
+
       </section>
 
       <section>
         <div className='mt-24 mb-10 text-2xl'>Projek Saya 🚀</div>
-
-        {projects.map(project => <Project name={project.name} description={project.description} link={project.link} stack={project.stack} />)}
+        {projects.map(project => <Project project={project} />)}
       </section>
     </div>
   )
--- src/Project.jsx
+++ src/Project.jsx
@@ -1,13 +1,13 @@
 const Project = (props) => {
   return (
     <div className='px-4 py-2 my-4 transition ease-in-out delay-75 border rounded-lg text-green-50 border-green-50 hover:text-green-100 hover:bg-green-800'>
-      <div className="mb-4 text-2xl font-semibold">{props.name}</div>
-      <p>{props.description}</p>
-      {props.link && <div className="mt-2">Link: <a href={props.link} target="_blank" className="underline">{props.link}</a></div>}
+      <div className="mb-4 text-2xl font-semibold">{props.project.name}</div>
+      <p>{props.project.description}</p>
+      <div>Tahun: {props.project.year}</div>
+      {props.project.link && <div className="mt-2">Link: <a href={props.project.link} target="_blank" className="underline">{props.project.link}</a></div>}
       <p className="mt-2 mb-3 font-semibold">Stack:</p>
       <ul className="flex gap-2">
-        {props.stack.map(item => <li className="px-2 py-0.5 text-sm bg-green-300 text-green-950 rounded-2xl">{item}</li>)}
-
+        {props.project.stack.map(item => <li className="px-2 py-0.5 text-sm bg-green-300 text-green-950 rounded-2xl">{item}</li>)}
       </ul>
     </div>
   )
